4ac2572c0d25803c180f7ad405f4355b216e4b42
zarttic <332209078@qq.com>
2024-07-20 19:12:32 +0800

    (Update gateway with new routing rules and header filters for payment service)

--- cloud-provider-payment/src/main/java/org/zartt/cloud/controller/PayGateWayController.java
+++ cloud-provider-payment/src/main/java/org/zartt/cloud/controller/PayGateWayController.java
@@ -1,13 +1,17 @@
 package org.zartt.cloud.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.zartt.cloud.entity.Pay;
 import org.zartt.cloud.resp.ResultData;
 import org.zartt.cloud.service.PayService;
+
+import java.util.Enumeration;
 
 /**
  * @author: zartt
@@ -30,5 +34,22 @@ public class PayGateWayController
     public ResultData<String> getGatewayInfo()
     {
         return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request)
+    {
+        StringBuilder result = new StringBuilder();
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements())
+        {
+            String headName = headers.nextElement();
+            String headValue = request.getHeader(headName);
+            System.out.println("请求头名: " + headName +"\t\t\t"+"请求头值: " + headValue);
+            if("X-Request-name".equalsIgnoreCase(headName)
+                    || "X-Request-id".equalsIgnoreCase(headName) ||"X-Request-change".equalsIgnoreCase(headName) ) {
+                result.append(headName).append("\t ").append(headValue).append(" ");
+            }
+        }
+        return ResultData.success("getGatewayFilter 过滤器 test： "+result+" \t "+ DateUtil.now());
     }
 }

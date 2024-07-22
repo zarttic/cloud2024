4442eab4d905cc97ef1dffe3577b7d0d794300cc
zarttic <332209078@qq.com>
2024-07-22 15:24:25 +0800

    (Initial commit for new module: cloudalibaba-sentinel-gateway & implement sentinel flow control for gateway)

---
+++ cloudalibaba-sentinel-gateway/src/main/java/org/zartt/cloud/Main9528.java
@@ -0,0 +1,17 @@
+package org.zartt.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: zartt
+ * @description: TODO
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main9528 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main9528.class,args);
+    }
+}

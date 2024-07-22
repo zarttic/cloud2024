9dfbf21b1c8da2f27567481171321d2a17333005
zarttic <332209078@qq.com>
2024-07-22 11:47:47 +0800

    (Update FlowLimitController with new break rules for slow call ratio, exception ratio, and exception count)

--- cloudalibaba-sentinel-service-8401/src/main/java/org/zartt/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service-8401/src/main/java/org/zartt/cloud/controller/FlowLimitController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zartt.cloud.service.FlowLimitService;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author: zartt
  * @description: TODO
@@ -43,6 +45,41 @@ public class FlowLimitController {
     {
         flowLimitService.common();
         return "------testD";
+    }
+    /**
+     * 新增熔断规则-慢调用比例
+     * @return
+     */
+    @GetMapping("/testF")
+    public String testF()
+    {
+        //暂停几秒钟线程
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println("----测试:新增熔断规则-慢调用比例 ");
+        return "------testF 新增熔断规则-慢调用比例";
+    }
+
+    /**
+     * 新增熔断规则-异常比例
+     * @return
+     */
+    @GetMapping("/testG")
+    public String testG()
+    {
+        System.out.println("----测试:新增熔断规则-异常比例 ");
+        int age = 10/0;
+        return "------testG,新增熔断规则-异常比例 ";
+    }
+    /**
+     * 新增熔断规则-异常数
+     * @return
+     */
+    @GetMapping("/testH")
+    public String testH()
+    {
+        System.out.println("----测试:新增熔断规则-异常数 ");
+        int age = 10/0;
+        return "------testH,新增熔断规则-异常数 ";
     }
 
 }
